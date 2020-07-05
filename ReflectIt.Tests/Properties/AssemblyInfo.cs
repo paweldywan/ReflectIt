921d23d45bfbaa6b7d2bae789d58e84023a63306
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-07-05 20:48:26 +0200

    Development

---
+++ ReflectIt.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReflectIt.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReflectIt.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("655dd9b7-2d5f-4b07-91fa-983734259698")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- ReflectIt/Program.cs
+++ ReflectIt/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,43 @@ namespace ReflectIt
     {
         static void Main(string[] args)
         {
+            var employeeList = CreateCollection(typeof(List<>), typeof(Employee));
+
+            Console.Write(employeeList.GetType().Name);
+
+            var genericArguments = employeeList.GetType().GenericTypeArguments;
+
+            foreach (var arg in genericArguments)
+            {
+                Console.Write("[{0}]", arg.Name);
+            }
+
+            Console.WriteLine();
+
+            var employee = new Employee();
+            var employeeType = typeof(Employee);
+            var methodInfo = employeeType.GetMethod("Speak");
+            methodInfo = methodInfo.MakeGenericMethod(typeof(DateTime));
+            methodInfo.Invoke(employee, null);
+
+            Console.ReadLine();
+        }
+
+        private static object CreateCollection(Type collectionType, Type itemType)
+        {
+            var closedType = collectionType.MakeGenericType(itemType); // Nie unbound type
+
+            return Activator.CreateInstance(closedType);
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public void Speak<T>()
+        {
+            Console.WriteLine(typeof(T).Name);
         }
     }
 }
